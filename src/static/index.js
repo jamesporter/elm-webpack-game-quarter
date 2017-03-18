7eff2de7bfc01c94a949981a03f39876b4ba8714
James Porter <james@amimetic.co.uk>
2017-03-18 09:59:50 +0000

    move over example game, wire up, deploy successfully with both S3 and now

--- src/static/index.js
+++ src/static/index.js
@@ -2,5 +2,5 @@
 require( './styles/main.styl' );
 
 // inject bundled Elm app into div#main
-var Elm = require( '../elm/Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+var Elm = require( '../elm/Game' );
+Elm.Game.embed( document.getElementById( 'main' ) );
